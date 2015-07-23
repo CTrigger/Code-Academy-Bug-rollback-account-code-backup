dc592fa8a6e16c0c9d96b74c3b7af22c7fc0693d
Ricardo Kim <hexadecimal.exe@gmail.com>
2015-07-23 00:27:36 -0300

    Create Loops & Iterators .rb

---
+++ Loops & Iterators .rb
@@ -0,0 +1,59 @@
+=begin
+Code-Academy-Bug-rollback-account-code-backup
+my account in code academy starting get some roll back so i solved "save" the progress
+=end
+#2/18
+
+i = 0
+while i < 5
+  puts i
+  i = i+ 1
+end
+
+#3/18
+counter = 1
+until counter > 10
+  puts counter
+  counter = counter + 1
+  # Add code to update 'counter' here!
+end
+
+#4/18
+counter = 1
+while counter < 11
+  puts counter
+  counter += 1
+end
+
+
+#6/18
+for num in 1..15
+  puts num
+end
+
+
+#7/18
+for num in 1..20
+  puts num
+end
+
+#8/18
+i = 20
+loop do
+  i -= 1
+  print "#{i}"
+  break if i <= 0
+end
+
+#9/18
+i = 20
+loop do
+  i -= 1
+  next if i % 2 == 1
+  print "#{i}"
+  break if i <= 0
+end
+
+
+#10/18
+my_array = [1,2,3,4,5]
